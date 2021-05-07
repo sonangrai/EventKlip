07b63486f5e0008aba115911f43a1e7e395ec46a
sonangrai <shonahangrae@gmail.com>
2021-05-07 10:17:13 +0545

    login and signup moda;s added

--- src/js/main.js
+++ src/js/main.js
@@ -25,4 +25,38 @@
   close.addEventListener("click", () => {
     mobBox.classList.remove("open");
   });
+
+  //Phone Codes
+  const input = document.querySelector("#phone");
+  intlTelInput(input, {
+    preferredCountries: ["np"],
+    separateDialCode: true,
+  });
+  const input2 = document.querySelector("#phone-sign");
+  intlTelInput(input2, {
+    preferredCountries: ["np"],
+    separateDialCode: true,
+  });
+
+  //Show Password
+  let showPass = document.querySelector(".eye");
+  let showPass2 = document.querySelector(".eye-signup");
+  let passInp = document.querySelector("#password");
+  let passInp2 = document.querySelector("#password-signup");
+  showPass.addEventListener("click", () => {
+    showPass.classList.toggle("show");
+    if (passInp.type === "password") {
+      passInp.type = "text";
+    } else {
+      passInp.type = "password";
+    }
+  });
+  showPass2.addEventListener("click", () => {
+    showPass2.classList.toggle("show");
+    if (passInp2.type === "password") {
+      passInp2.type = "text";
+    } else {
+      passInp2.type = "password";
+    }
+  });
 })();
