6a0f8f445623fa8aa4bd7cfea972b2f3093f28f5
sonangrai <shonahangrae@gmail.com>
2021-05-14 09:31:14 +0545

    added the sidebar menu responsive

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -2,4 +2,10 @@
 
 (function () {
   console.log("Have a good day 🎈");
+
+  var dashNav = document.querySelector("#dash-nav");
+  dashNav.addEventListener("click", () => {
+    document.querySelector("aside").classList.toggle("show");
+    dashNav.classList.toggle("show");
+  });
 })();
